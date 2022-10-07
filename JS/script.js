d482bbc46e40cb2b06983ef7eaa2de365acdf3f0
EGREG0R-Dmitriy <113848357+EGREG0R-Dmitriy@users.noreply.github.com>
2022-10-07 22:13:31 +0300

    мій сайт

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+// let userName = prompt( "Хто там ", '');
+
+// if (userName === 'Admin') {
+
+//     let pass = prompt ('Пароль?', '');
+
+//     if (pass === 'Пароль') {
+//         alert ('Лакаво просимо!');
+//     } else if (pass === '' || pass === null) {
+//         alert( 'Скасовано');
+//     } else {
+//         alert ( 'Не правильний пароль');
+//     }
+
+// } else if (userName === '' || userName === null) {
+//     alert ( 'Скасовано' );
+// } else {
+//     alert ( 'Я вас не знаю' );
+// }g
+ 33
+let ask = prompt("hi", '');
+// alert (ask);
+alert (ask ?? "хз хто");
